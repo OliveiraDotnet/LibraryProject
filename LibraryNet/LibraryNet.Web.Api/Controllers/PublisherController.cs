c1d4e2279c070319a7a2d0a9fbce7fd0817179a9
Matheus oliveira <matheus.oliveira@vibetecnologia.com>
2024-08-27 14:26:53 -0300

    - Ajuste API ignorar ciclos - Ajustes controllers e inclusão de métodos

--- LibraryNet/LibraryNet.Web.Api/Controllers/PublisherController.cs
+++ LibraryNet/LibraryNet.Web.Api/Controllers/PublisherController.cs
@@ -3,27 +3,26 @@ using LibraryNet.Core.Models;
 using LibraryNet.Core.Models.VO;
 using LibraryNet.Utils;
 using LibraryNet.Utils.Extensions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace LibraryNet.Web.Api.Controllers
 {
-    [SwaggerTag("Manages processes related to Authors")]
+    [SwaggerTag("Manages processes related to Publishing Companies")]
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthorController : ControllerBase
+    public class PublisherController : ControllerBase
     {
-        protected IAuthorServices Services { get; }
-        protected ILogger<AuthorController> Log { get; }
-        public AuthorController(IAuthorServices services, ILogger<AuthorController> log)
+        protected IPublisherServices Services { get; }
+        protected ILogger<PublisherController> Log { get; }
+        public PublisherController(IPublisherServices services, ILogger<PublisherController> log)
         {
             Services = services;
             Log = log;
         }
         [HttpGet("GetAll")]
-        [SwaggerOperation(Summary = "Get all Authors", Description = "")]
-        [SwaggerResponse(200, "Success", typeof(List<Author>))]
+        [SwaggerOperation(Summary = "Get all Publishers", Description = "")]
+        [SwaggerResponse(200, "Success", typeof(List<Publisher>))]
         [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
         [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
         public async Task<IActionResult> GetAllAsync()
@@ -33,15 +32,15 @@ namespace LibraryNet.Web.Api.Controllers
         }
 
         [HttpPost("Create")]
-        [SwaggerOperation(Summary = "Create a Author", Description = "")]
-        [SwaggerResponse(200, "Author created with Success", typeof(string))]
+        [SwaggerOperation(Summary = "Create a PublishCompany", Description = "")]
+        [SwaggerResponse(200, "PublishCompany created with Success", typeof(string))]
         [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
         [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
-        public async Task<IActionResult> CreateAuthorAsync(AuthorVO AuthorVO)
+        public async Task<IActionResult> CreatePublishCompanyAsync(PublisherVO PublishCompanyVO)
         {
-            var author = AuthorVO.Like<Author>();
-            await Services.CreateAsync(author);
-            return Ok(author.Name);
+            var publishCompany = PublishCompanyVO.Like<Publisher>();
+            await Services.CreateAsync(publishCompany);
+            return Ok(publishCompany.Name);
         }
     }
 }
--- LibraryNet/LibraryNet.Web.Api/Program.cs
+++ LibraryNet/LibraryNet.Web.Api/Program.cs
@@ -1,34 +1,42 @@
-using LibraryNet.Utils;
 using LibraryNet.Utils.DependencyInjection;
 using LibraryNet.Web.Api.Application;
+using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-ApplicationInitializer.Init(builder.Services)
-                      .SetMappingManager<AutoMapperManager>()
-                      .SetDependencyManager<Factory>()
-                      .Finish();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); ; 
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+
+        ApplicationInitializer.Init(builder.Services)
+                              .SetMappingManager<AutoMapperManager>()
+                              .SetDependencyManager<Factory>()
+                              .Finish();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseExceptionHandler("/error");
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-app.UseExceptionHandler("/error");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
