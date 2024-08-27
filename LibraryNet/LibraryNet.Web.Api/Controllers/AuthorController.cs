31224cda54853289aeddfcc984c05392dec71659
Matheus oliveira <matheus.oliveira@vibetecnologia.com>
2024-08-27 15:29:29 -0300

    Ajuste controllers autor e editora

--- LibraryNet/LibraryNet.Web.Api/Controllers/AuthorController.cs
+++ LibraryNet/LibraryNet.Web.Api/Controllers/AuthorController.cs
@@ -23,13 +23,13 @@ namespace LibraryNet.Web.Api.Controllers
         }
         [HttpGet("GetAll")]
         [SwaggerOperation(Summary = "Get all Authors", Description = "")]
-        [SwaggerResponse(200, "Success", typeof(List<Author>))]
+        [SwaggerResponse(200, "Success", typeof(List<AuthorVO>))]
         [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
         [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
         public async Task<IActionResult> GetAllAsync()
         {
             var result = await Services.GetAllAsync();
-            return Ok(result);
+            return Ok(result.Like<List<AuthorVO>>());
         }
 
         [HttpPost("Create")]
@@ -42,6 +42,28 @@ namespace LibraryNet.Web.Api.Controllers
             var author = AuthorVO.Like<Author>();
             await Services.CreateAsync(author);
             return Ok(author.Name);
+        }
+
+        [HttpPut("Update")]
+        [SwaggerOperation(Summary = "Update a Author", Description = "")]
+        [SwaggerResponse(200, "Book updated with Success", typeof(string))]
+        [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
+        [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
+        public async Task<IActionResult> UpdateAsync(Author author)
+        {
+            await Services.UpdateAsync(author);
+            return Ok(author.Name);
+        }
+
+        [HttpDelete("Delete/{authorId}")]
+        [SwaggerOperation(Summary = "Delete a Author", Description = "")]
+        [SwaggerResponse(200, "Deleted with success", typeof(string))]
+        [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
+        [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
+        public async Task<IActionResult> DeleteAsync(string authorId)
+        {
+            await Services.DeleteAsync(authorId);
+            return Ok(authorId);
         }
     }
 }
--- LibraryNet/LibraryNet.Web.Api/Controllers/PublisherController.cs
+++ LibraryNet/LibraryNet.Web.Api/Controllers/PublisherController.cs
@@ -22,13 +22,13 @@ namespace LibraryNet.Web.Api.Controllers
         }
         [HttpGet("GetAll")]
         [SwaggerOperation(Summary = "Get all Publishers", Description = "")]
-        [SwaggerResponse(200, "Success", typeof(List<Publisher>))]
+        [SwaggerResponse(200, "Success", typeof(List<PublisherVO>))]
         [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
         [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
         public async Task<IActionResult> GetAllAsync()
         {
             var result = await Services.GetAllAsync();
-            return Ok(result);
+            return Ok(result.Like<List<PublisherVO>>());
         }
 
         [HttpPost("Create")]
@@ -41,6 +41,28 @@ namespace LibraryNet.Web.Api.Controllers
             var publishCompany = PublishCompanyVO.Like<Publisher>();
             await Services.CreateAsync(publishCompany);
             return Ok(publishCompany.Name);
+        }
+
+        [HttpPut("Update")]
+        [SwaggerOperation(Summary = "Update a Publisher", Description = "")]
+        [SwaggerResponse(200, "Book updated with Success", typeof(string))]
+        [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
+        [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
+        public async Task<IActionResult> UpdateAsync(Publisher publisher)
+        {
+            await Services.UpdateAsync(publisher);
+            return Ok(publisher.Name);
+        }
+
+        [HttpDelete("Delete/{publisherId}")]
+        [SwaggerOperation(Summary = "Delete a Publisher", Description = "")]
+        [SwaggerResponse(200, "Deleted with success", typeof(string))]
+        [SwaggerResponse(400, "Bad Request", typeof(ErrorResponse))]
+        [SwaggerResponse(500, "Internal Server Error", typeof(ErrorResponse))]
+        public async Task<IActionResult> DeleteAsync(string publisherId)
+        {
+            await Services.DeleteAsync(publisherId);
+            return Ok(publisherId);
         }
     }
 }
